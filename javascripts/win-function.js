e066a2d2ee7b33710fc1eb7c996609d49a7b4345
Forrest Venable & Liorr Yehuda Morrison <pair+forrestvenable.liorrm@devbootcamp.com>
2014-12-18 13:37:55 -0800

    write 'find elements' functions for win condition

--- javascripts/win-function.js
+++ javascripts/win-function.js
@@ -19,13 +19,38 @@ var color = function(cell) {
 }
 
 var findRow = function(rowClass) {
+  return document.getElementsByClassName(rowClass)
+}
+
+var findColumn = function(colClass) {
+  var list = document.getElementsByClassName(colClass)[0].children
+  var array = []
+  for (var i = 0; i<=list.length; i++) {
+    array[ i ] = list[list.length-i]
+  }
+  array.shift();
+  return array
+}
+
+var findCell = function(col, row) {
+  var column = document.getElementsByClassName("c"+col)[0].children
+  var solution
+  for (var i=0; i<column.length; i++) {
+    if (column[i].classList.contains("r"+row)) {
+      solution = column[i];
+    }
+  }
+  solution
+}
+
+var findDiagonal1 = function(cell) {
+  x = parseInt(row(cell)[1]);
+  y = parseInt(column(cell)[1]);
 
 }
 
-
-
 var checkRow = function(cell) {
-
+  rowArray = findRow(row(cell))
 }
 
 var checkColumn = function(cell) {
