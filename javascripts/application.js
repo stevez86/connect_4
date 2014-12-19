081fd8b0aa6b671a749a8358cf7a54319a2a8447
Joe Zuiches & Steve Zimmerman <pair+jzuiches.stevez86@devbootcamp.com>
2014-12-18 17:11:02 -0800

    board updated

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,3 +1,32 @@
-$(.column).click(columnClick)
+// $(document).ready(function(){
+
+var playerTurn = "red";
 
 
+var changeTurns = function () {
+  if ( playerTurn == "red" ) {
+    playerTurn = "black";
+  } else {
+    playerTurn = "red";
+  }
+
+  $( ".player" ).attr('data-color',playerTurn);
+}
+
+var placePiece = function() {
+  $( this ).children("[data-color=empty]").last().attr('data-color',playerTurn);
+  changeTurns();
+}
+
+
+$( ".column" ).click(placePiece);
+
+
+// $( ".img" ).click(placePiece);
+// var placePiece = function(){
+//   console.log("test")
+// }
+// $(".board").on("click",'.column.c0', function(e){
+//   console.log("test")
+// })
+// });
